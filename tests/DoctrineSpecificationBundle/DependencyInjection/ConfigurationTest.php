73c4ca1a202abb3c6047f45b3665dee3a16d4776
Gilles <contact@gb-prod.fr>
2017-06-28 10:32:28 +0200

    chore: up to php7

--- tests/DoctrineSpecificationBundle/DependencyInjection/ConfigurationTest.php
+++ tests/DoctrineSpecificationBundle/DependencyInjection/ConfigurationTest.php
@@ -2,15 +2,16 @@
 
 namespace Tests\GBProd\DoctrineSpecificationBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use GBProd\DoctrineSpecificationBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 /**
  * Tests for Configuration
  *
  * @author gbprod <contact@gb-prod.fr>
  */
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends TestCase
 {
     public function testGetConfigTreeBuilder()
     {
